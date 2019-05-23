51f74cf8d878e9dfce806161bfcbbe9399673a7c
Tracey <traceyphan95@gmail.com>
2019-05-23 19:14:40 +0000

    Taskbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_145045) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "quotes", force: :cascade do |t|
-    t.string "saying"
-    t.string "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
